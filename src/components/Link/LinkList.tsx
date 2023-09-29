f16ad74bfb09d1aa34d51ea158934962c71004ad
Jannik Zschiesche <j.zschiesche@21torr.com>
2023-09-29 11:37:52 +0200

    Add styling for package links

--- src/components/Link/LinkList.tsx
+++ src/components/Link/LinkList.tsx
@@ -1,6 +1,29 @@
 import React, {ReactElement} from "react";
 import Link from "@docusaurus/Link";
 
+import styles from "./LinkList.module.css";
+
+// @ts-ignore PNG imports work, they are imported as data url
+import packagistLogo from "./icons/packagist.png";
+import GithubLogo from "./icons/github.svg";
+import NpmLogo from "./icons/npm.svg";
+
+/**
+ * Parses a package name from a URL
+ */
+function parsePackageName (url: string)
+{
+	const match = /\/(?<name>[^\/]+\/[^\/]+)\/?$/.exec(url);
+
+	if (!match)
+	{
+		throw new Error(`Could not parse package URL: ${url}`);
+	}
+
+	return match.groups.name;
+}
+
+
 export interface LinkListProps
 {
 	npm?: string;
@@ -15,16 +38,46 @@ export function LinkList (props: LinkListProps): ReactElement | null
 		return null;
 	}
 
+	const composerPackage = props.packagist
+		? parsePackageName(props.packagist)
+		: undefined;
+	const npmPackage = props.npm
+		? parsePackageName(props.npm)
+		: undefined;
+
 	return (
-		<ul>
+		<ul className={styles.linklist}>
 			{props.github && (
-				<li><Link to={props.github}>Github</Link></li>
+				<li>
+					<Link
+						to={props.github}
+						className={styles.link}
+					>
+						<span className={styles.icon}><GithubLogo /></span> Github
+					</Link>
+				</li>
 			)}
 			{props.npm && (
-				<li><Link to={props.npm}>npm</Link></li>
+				<li>
+					<Link
+						to={props.npm}
+						className={styles.link}
+						title="Link to npm"
+					>
+						<code className={styles.code}><span className={`${styles.icon} ${styles.npmIcon}`}><NpmLogo /></span> {npmPackage}</code>
+					</Link>
+				</li>
 			)}
 			{props.packagist && (
-				<li><Link to={props.packagist}>packagist</Link></li>
+				<li>
+					<Link
+						to={props.packagist}
+						className={styles.link}
+						title="Link to packagist.org"
+					>
+						<code className={styles.code}><span className={styles.icon}><img src={packagistLogo} alt="" /></span> {composerPackage}</code>
+					</Link>
+				</li>
 			)}
 		</ul>
 	);
