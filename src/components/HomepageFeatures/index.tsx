a13622ae438a30008d89eec63b4dec3a1413c9c6
Jannik Zschiesche <j.zschiesche@21torr.com>
2024-04-30 11:03:31 +0200

    Make border radius explicit

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -18,7 +18,7 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'All in one place',
-    image: <img src={placeImage} alt="Photo from coffee house in Jakarta, Indonesia" />,
+    image: <img src={placeImage} alt="Photo from coffee house in Jakarta, Indonesia" className={styles.featureImage} />,
     description: (
       <>
         This website contains all documentation for all of our open-source tools.
@@ -27,7 +27,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: 'Help appreciated',
-    image: <img src={helpImage} alt="Photo of a building during construction" />,
+    image: <img src={helpImage} alt="Photo of a building during construction" className={styles.featureImage} />,
     description: (
       <>
           The docs can be edited by everyone. Feel welcome to write docs or create issues about missing / unclear topics in the <Link href="https://github.com/21TORR/docs">repository</Link>.
@@ -36,7 +36,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: 'Work with us',
-    image: <img src={joinImage} alt="Photo from Stuttgart, Germany" />,
+    image: <img src={joinImage} alt="Photo from Stuttgart, Germany" className={styles.featureImage} />,
     description: (
       <>
           You like our components? Come <Link href="https://www.21torr.com/career">join our team</Link> and work with us to make them even better.
@@ -48,7 +48,7 @@ const FeatureList: FeatureItem[] = [
 function Feature({title, image, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
-      <div className={`text--center ${styles.featureImage}`}>{image}</div>
+      <div className={`text--center ${styles.featureImageContainer}`}>{image}</div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
